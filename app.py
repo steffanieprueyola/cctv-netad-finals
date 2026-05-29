114ad249c4124f7b78f8b785912d0d486b3453de
steffanieprueyola <steffanieprueyola@gmail.com>
2026-05-29 12:12:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,8 @@ def stream_proxy(filename):
 
     session = requests.Session()
     session.headers.update({
-        "ngrok-skip-browser-warning": "true"
+        "ngrok-skip-browser-warning": "true",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
     })
 
     try:
