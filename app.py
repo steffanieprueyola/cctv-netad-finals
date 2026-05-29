2a98c0d963ae57ea0dabfb217e4fff106e3904d4
steffanieprueyola <steffanieprueyola@gmail.com>
2026-05-29 13:40:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def stream_proxy(filename):
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept": "application/vnd.apple.mpegurl, application/x-mpegURL, */*",
     "Accept-Language": "en-US,en;q=0.9",
-    "Authorization": "Bearer mysecret123",
+    "Authorization": "Bearer nyawasan123",
 })
 
     try:
